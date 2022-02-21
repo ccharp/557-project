6c55a47bd4dadb8bada7f233d82e04654221dccc
Marie Hoeger <marie.hoeger@gmail.com>
2022-02-21 12:27:06 -0800

    add normalized co2 (#3)

--- get_data.py
+++ get_data.py
@@ -2,30 +2,71 @@ import os
 import urllib.request
 import zipfile
 
-DATA_DIR = 'data'
-WDI_XLSX = os.path.join(DATA_DIR, 'WDIEXCEL.xlsx') # world development indicators
-WDI_ZIP = os.path.join(DATA_DIR, 'WDIEXCEL.zip') 
-CT_CSV = os.path.join(DATA_DIR, 'CT.csv') # world development indicators
+DATA_DIR = "data"
+WDI_XLSX = os.path.join(DATA_DIR, "WDIEXCEL.xlsx")  # world development indicators
+WDI_ZIP = os.path.join(DATA_DIR, "WDIEXCEL.zip")
+CT_CSV = os.path.join(DATA_DIR, "CT.csv")  # world development indicators
+NORMALIZED_CO2_DATA = os.path.join(DATA_DIR, "NORMALIZED_CO2_DATA.csv")
 
 if not os.path.isdir(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-if not os.path.exists(WDI_XLSX):
-    zip_path = os.path.join(DATA_DIR, WDI_ZIP)
-    print('Downloading WDI dataset...')
 
-    # We go for the Excel doc here because server kept resetting the connection when I tried to download the CSV
-    # This reproduced in browser too *shrug*
-    urllib.request.urlretrieve('https://databank.worldbank.org/data/download/WDI_excel.zip' , WDI_ZIP)
+def fetch_data(
+    destination_path: str,
+    dataset_name: str,
+    download_url: str,
+    is_zip: bool = False,
+    unzipped_path: str = None,
+):
+    """
+    Downloads data from source into DATA_DIR
+    Args:
+        destination_path:
+            Path to download asset from download_url to
+        dataset_name:
+            Name of dataset (for progress messages)
+        download_url:
+            URL to get dataset from
+        is_zip:
+            Optional bool if downloaded asset is a zip file
+        unzipped_path:
+            Optional string to specify path of unzipped asset.
+            Used for checking if we should download the zip asset again.
+    """
+    if os.path.exists(unzipped_path or destination_path):
+        print(f"{dataset_name} dataset exists... skipping download")
+    else:
+        print(f"Downloading {dataset_name} dataset...")
+        # We go for the Excel doc here because server kept resetting the connection when I tried to download the CSV
+        # This reproduced in browser too *shrug*
+        urllib.request.urlretrieve(download_url, destination_path)
 
-    print('Unzipping WDI...')
-    with zipfile.ZipFile(WDI_ZIP, 'r') as z:
-        z.extractall(DATA_DIR)
+        if is_zip:
+            print(f"Unzipping {destination_path}...")
+            with zipfile.ZipFile(destination_path, "r") as z:
+                z.extractall(DATA_DIR)
+            os.remove(destination_path)
+        print("Done!")
 
-    os.remove(WDI_ZIP)
 
-if not os.path.exists(os.path.join(DATA_DIR, CT_CSV)):
-    print('Download CT data...')
-    urllib.request.urlretrieve('https://api.climatetrace.org/emissions_by_subsector_timeseries?interval=year&since=2015&to=2020&download=csv' , CT_CSV)
+# Fetch datasets
+fetch_data(
+    WDI_ZIP,
+    "WDI",
+    "https://databank.worldbank.org/data/download/WDI_excel.zip",
+    True,
+    WDI_XLSX,
+)
+fetch_data(
+    CT_CSV,
+    "CT",
+    "https://api.climatetrace.org/emissions_by_subsector_timeseries?interval=year&since=2015&to=2020&download=csv",
+)
+fetch_data(
+    NORMALIZED_CO2_DATA,
+    "Normalized CO2",
+    "https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv",
+)
 
-print('Data successfully downloaded to ./data')
+print("Data successfully downloaded to ./data")
